11690a50c681508e8074900cc41df18c1c3c67da
Kushagra <35634467+kshandy@users.noreply.github.com>
2019-11-05 17:41:47 +0530

    added sounds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
 import key1 from './sounds/key1.wav';
+import key2 from './sounds/key2.wav';
+import key3 from './sounds/key3.wav';
+import key4 from './sounds/key4.wav';
+import key5 from './sounds/key5.wav';
+import key6 from './sounds/key6.wav';
 
 const SOUNDKIT = [
-  {id: 'key1', name: 'Amen Break', src: key1},
+  {id: 'key', name: 'Amen Break', src: key1},
+  {id: 'key', name: 'Funky Break', src: key2},
+  {id: 'key', name: 'Leevee Break', src: key3},
+  {id: 'key', name: 'President Break', src: key4},
+  {id: 'key', name: 'Synthetic Break', src: key5},
+  {id: 'key', name: 'Skull Break', src: key6},
 ];
 
 class Key extends React.Component {
@@ -19,9 +29,9 @@ class Key extends React.Component {
 
   render () {
     return (
-      <div className = {this.props.id} onClick = {this.handleClick}>
-        <h1>Amen Break</h1>
-        <audio id = {this.props.id} src = {this.props.soundsrc} ref = {ref => this.sound = ref}></audio>
+      <div className = {this.props.sound.id} onClick = {this.handleClick}>
+        <h1>{this.props.kname}</h1>
+        <audio id = {this.props.sound.id} src = {this.props.sound.src} ref = {ref => this.sound = ref}></audio>
       </div>
     );
   }
@@ -32,7 +42,7 @@ class App extends React.Component {
     return (
       <div className = "App">
         {SOUNDKIT.map(sound => {
-          return (<Key kid = {sound.id} kname = {sound.name} soundsrc = {sound.src} />)
+          return (<Key sound = {sound} />)
         })}        
       </div>
     );
