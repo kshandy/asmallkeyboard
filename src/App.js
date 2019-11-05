e458d1b2f1489cb943ed32304d52b85fec8397f5
Kushagra <35634467+kshandy@users.noreply.github.com>
2019-11-05 11:23:56 +0530

    asmallkeyboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import key1 from './sounds/key1.wav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SOUNDKIT = [
+  {id: 'key1', name: 'Amen Break', src: key1},
+];
+
+class Key extends React.Component {
+  constructor (props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.sound.play();
+    this.sound.currentTime = 0;
+  }
+
+  render () {
+    return (
+      <div className = {this.props.id} onClick = {this.handleClick}>
+        <h1>Amen Break</h1>
+        <audio id = {this.props.id} src = {this.props.soundsrc} ref = {ref => this.sound = ref}></audio>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className = "App">
+        {SOUNDKIT.map(sound => {
+          return (<Key kid = {sound.id} kname = {sound.name} soundsrc = {sound.src} />)
+        })}        
+      </div>
+    );
+  }
 }
 
 export default App;
