c6db8c0bf0ed8fa41244e19b18fa602f68cff601
Kushagra <35634467+kshandy@users.noreply.github.com>
2019-11-06 10:44:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import './App.css';
-import key1 from './sounds/key1.wav';
-import key2 from './sounds/key2.wav';
-import key3 from './sounds/key3.wav';
-import key4 from './sounds/key4.wav';
-import key5 from './sounds/key5.wav';
-import key6 from './sounds/key6.wav';
-
-const SOUNDKIT = [
-  {id: 'key', name: 'Amen Break', src: key1},
-  {id: 'key', name: 'Funky Break', src: key2},
-  {id: 'key', name: 'Leevee Break', src: key3},
-  {id: 'key', name: 'President Break', src: key4},
-  {id: 'key', name: 'Synthetic Break', src: key5},
-  {id: 'key', name: 'Skull Break', src: key6},
-];
+import SOUNDKIT from './Sound';
 
 class Key extends React.Component {
   constructor (props) {
@@ -24,13 +10,14 @@ class Key extends React.Component {
 
   handleClick() {
     this.sound.play();
+    this.sound.volume = 0.25;
     this.sound.currentTime = 0;
   }
 
   render () {
     return (
       <div className = {this.props.sound.id} onClick = {this.handleClick}>
-        <h1>{this.props.kname}</h1>
+        <h1>{this.props.sound.kname}</h1>
         <audio id = {this.props.sound.id} src = {this.props.sound.src} ref = {ref => this.sound = ref}></audio>
       </div>
     );
